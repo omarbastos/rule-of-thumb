c1e891a6158d3b867a24660b75a7c8c00652b1c8
Omar Hernandez <ohernandez@hellobuild.co>
2021-12-19 21:29:06 -0400

    Remove unused aria-*

--- src/components/base/PollBars/index.tsx
+++ src/components/base/PollBars/index.tsx
@@ -26,7 +26,6 @@ const PollBars = ({ upVotes, downVotes, square, name }: Props) => {
         aria-valuemin={0}
         aria-valuemax={100}
         aria-label={`${name} Up Votes`}
-        aria-progressbar-name={`${name} Up Votes`}
       >
         <div className="flex items-center px-4">
           <ThumbsUpIcon
@@ -45,7 +44,6 @@ const PollBars = ({ upVotes, downVotes, square, name }: Props) => {
         aria-valuemin={0}
         aria-valuemax={100}
         aria-label={`${name} Down Votes`}
-        aria-progressbar-name={`${name} Down Votes`}
         className="shadow-none flex justify-end items-center whitespace-nowrap text-white bg-thumb-down/60"
       >
         <div className="flex items-center px-4">
